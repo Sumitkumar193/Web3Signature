5125c9dc010e9fade681354d0f29a0e2d0332442
sumitkumar33 <sumit.kumar@hestabit.in>
2022-10-22 23:30:21 +0530

    Added service provider

---
+++ src/Provider/Web3SignatureProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Desnake\Web3signature\Provider;
+
+use Illuminate\Support\ServiceProvider;
+
+class Web3SignatureProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
