d3f3c7dfbf4efe836c6e4505d9f884613be3f4ea
sumitkumar33 <sumit.kumar@hestabit.in>
2022-10-23 20:17:04 +0530

    Added verifyAddress method and comments

--- src/Web3ForContracts.php
+++ src/Web3ForContracts.php
@@ -3,19 +3,28 @@
 namespace Desnake\Web3signature;
 
 use kornrunner\Keccak;
+use Illuminate\Support\Str;
 use Desnake\Web3signature\Signature;
 
 trait Web3ForContracts
 {
-    // Build your next great package.
-    public function personal_ecRecover($msg, $signed)
+    /**
+     * @method Returns the address extracted from signature
+     * @param string $msg - Message that is signed 
+     * @param string $signed - Signature string with signed message
+     */
+    public function recoverAddress($msg, $signed)
     {
         $personal_prefix_msg = "\x19Ethereum Signed Message:\n" . strlen($msg) . $msg;
         $hex = $this->keccak256($personal_prefix_msg);
-        return $this->ecRecover($hex, $signed);
+        return $this->_recoverAddress($hex, $signed);
     }
 
-    public function ecRecover($hex, $signed)
+    /**
+     * @method extracts r, s, and v values from signed message
+     * @return string address from the signed message
+     */
+    public function _recoverAddress($hex, $signed)
     {
         $rHex   = substr($signed, 2, 64);
         $sHex   = substr($signed, 66, 64);
@@ -45,12 +54,35 @@ trait Web3ForContracts
         return '0x' . substr($this->keccak256(hex2bin($publicKeyString)), -40);
     }
 
+    /**
+     * @method checks the address with help of a signed message
+     * @param string $message - plaintext message that is signed
+     * @param string $signature - signed message hex data
+     * @param string $address - address to be verified
+     * @return boolean true if signature address and provided address is same.
+     */
+    public function verifyAddress($message, $signature, $address)
+    {
+        $recoverAddress = Str::lower($this->recoverAddress($message, $signature));
+        if (Str::lower($address) == $recoverAddress) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @return hex of a string.
+     */
     public function strToHex($string)
     {
         $hex = unpack('H*', $string);
         return '0x' . array_shift($hex);
     }
 
+    /**
+     * @return string Keccak256 hash of a given string
+     */
     public function keccak256($str)
     {
         return '0x' . Keccak::hash($str, 256);
